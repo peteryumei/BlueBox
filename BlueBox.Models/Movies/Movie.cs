1f3dcf510362010497980387ea573120ccb65e88
Peter Mei <peter.mei@showmgr.com>
2018-01-16 14:10:10 -0800

    Add Models project

---
+++ BlueBox.Models/Movies/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBox.Models.Movies
+{
+    class Movie
+    {
+        public int movie_id { get; set; }
+        public int movie_name { get; set; }
+    }
+}
